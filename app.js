dfde2915908ec5fbbfc574a02ef85f9e96224d8d
Ricardo Mercado <ricardoamercado@gmail.com>
2018-12-01 14:06:09 -0400

    Add message filters

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGO_HOSTNAME; // Connection URL
+const dbName = 'gotenacious'; // Database Name
 
-// Connection URL
-const url = process.env.MONGO_HOSTNAME;
-// Database Name
-const dbName = 'gotenacious';
 app.use(express.json());
+app.set('view engine', 'pug')
+
+
 app.get('/', (req, res) => { 
+
+  let filter = req.query.type ? { type: req.query.type } : {}
   // Get all messages
   MongoClient.connect(url, function(err, client) {
-    // assert.equal(null, err);
     if (err) {
       console.error('An error occurred connecting to MongoDB: ', err);
     } else {
       console.log("Connected successfully to server");
       const db = client.db(dbName);
-      
-      getMessages(db, (messages) => {
-        client.close();
-        res.send(messages)
+
+      getMessages(db, filter, (messages) => {
+        client.close()
+
+        res.render('dashboard', { title: 'GoTenatious Mesh Dashboard', messages: messages })
       })
     }
-    
   });
-  // res.sendFile(__dirname + '/templates/dashboard.html')
 })
 
 app.post('/message', (req, res) => { 
@@ -50,21 +51,16 @@ app.post('/message', (req, res) => {
 
 app.listen(port, () => console.log(`Dashboard listening on port ${port}!`))
 
-
 const insertMessage = function(db, message, callback) {
-  // console.log(rb)
   const collection = db.collection('messages')
   collection.insertOne(message)
   callback();
 }
 
-const getMessages = function(db, callback) {
+const getMessages = function(db, filter, callback) {
   // Get the documents collection
   const collection = db.collection('messages');
-  // Find some documents
-  collection.find({}).toArray(function(err, messages) {
-    console.log("Found the following messages");
-    console.log(messages)
+  collection.find(filter).toArray(function(err, messages) {
     callback(messages);
   });
 }
---
+++ js/filterMessages.js
@@ -0,0 +1,4 @@
+function filter(type) {
+  // window.location = `${process.env.BASE_URL}${type}`
+  window.location = `http://localhost:3000/?type=${type}`
+}
